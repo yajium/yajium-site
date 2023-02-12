5bd459ffcc3760f4f5c57de37f8d16838b01ed7c
yajium <117247060+yajium@users.noreply.github.com>
2023-02-13 00:15:05 +0900

    feat: navigation

---
+++ src/components/NavigationMenu.tsx
@@ -0,0 +1,64 @@
+import {
+  ChevronDoubleDownIcon,
+  ListBulletIcon,
+} from "@heroicons/react/24/outline";
+
+const NavigationMenu = () => {
+  return (
+    <nav className="px-4 py-4 flex justify-end lg:hidden">
+      <button
+        id="dropdownHoverButton"
+        data-dropdown-toggle="dropdownHover"
+        data-dropdown-trigger="hover"
+        className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+      >
+        <ListBulletIcon className="h-10 w-10 text-stone-300" />
+      </button>
+      <div
+        id="dropdownHover"
+        className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+      >
+        <ul
+          className="py-2 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownHoverButton"
+        >
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Settings
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Earnings
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Sign out
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationMenu;
