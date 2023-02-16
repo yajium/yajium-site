cbab93c2b5b42fba9bc7b71fe9ea5fd8b518e628
yajium <117247060+yajium@users.noreply.github.com>
2023-02-16 18:45:14 +0900

    fix: clean files

---
+++ src/constants/constant.ts
@@ -0,0 +1,92 @@
+export const SITE_TITLE = "yajium's site";
+export const SITE_DESCRIPTION = "おはこんちゃろ";
+
+export const snslinks = [
+  {
+    linkTitle: "Twitter",
+    name: "yajium_",
+    link: "https://twitter.com/yajium_",
+  },
+  {
+    linkTitle: "Github",
+    name: "yajium",
+    link: "https://github.com/yajium",
+  },
+  {
+    linkTitle: "Zenn",
+    name: "rorisutarou",
+    link: "https://zenn.dev/rorisutarou",
+  },
+  {
+    linkTitle: "SpeakerDeck",
+    name: "aaa",
+    link: "xxx",
+  },
+];
+
+export const myLikes = [
+  {
+    cate: "Tech",
+    detail: ["React", "Next.js", "Typescript", "C#"],
+  },
+  {
+    cate: "Hobby",
+    detail: ["Hikikomori"],
+  },
+  {
+    cate: "Music",
+    detail: ["ヨルシカ"],
+  },
+  {
+    cate: "Others",
+    detail: ["ぬいぐるみ", "ポケモン", "パグちゃん", "冬"],
+  },
+];
+
+export const myLife = [
+  {
+    date: "2022年4月～",
+    detail: "某社で社会人",
+  },
+  {
+    date: "2018年4月～",
+    detail: "某塾で大学生",
+  },
+  {
+    date: "1999年7月～",
+    detail: "某所で誕生",
+  },
+];
+
+export const homeCards = [
+  {
+    itemName: "About",
+    description: "yajiumのことを紹介しています",
+    href: "/about",
+  },
+  {
+    itemName: "Works",
+    description: "へんてこな作ったものたちを紹介しています",
+    href: "/works",
+  },
+  {
+    itemName: "Blog",
+    description: "頑張って文字を書いています",
+    href: "blog",
+  },
+];
+
+export const homeTitle = "yajium's site";
+
+export const homeDesc = `こんにちは、yajiumです。
+  ここまで見に来てくださり、ありがとうございます。
+
+  ___以下読まなくて大丈夫です___
+
+  今は東京に住んでおり、毎日ぬいぐるみたちと穏やかに暮らしています。
+  最近の趣味はTwitterでパグを見て癒されることです。
+
+  今の目標はソフトウェアエンジニアになることで、その目標に向けて日々を生きています。
+  Webの技術が好きで、最近はフロントエンドへの興味が強いです。ただ、仕事ではC#を触っていて主にバックエンドをやっています。
+
+  海と桜が好きなので、このサイトのデザインに要素を散りばめました🐳`;
--- src/consts.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = "yajium's site";
-export const SITE_DESCRIPTION = "おはこんちゃろ";
