53570c1d9dde5771d3cd6e6c4045f64a59e6edb3
yajium <117247060+yajium@users.noreply.github.com>
2023-02-18 11:35:51 +0900

    fix: articles type

---
+++ src/content/schemas/article.ts
@@ -0,0 +1,16 @@
+import { z } from "astro/zod";
+
+export const articleSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  // Transform string to Date object
+  pubDate: z
+    .string()
+    .or(z.date())
+    .transform((val) => new Date(val)),
+  // updatedDate: z
+  //   .string()
+  //   .optional()
+  //   .transform((str) => (str ? new Date(str) : undefined)),
+  tags: z.array(z.string()),
+});
