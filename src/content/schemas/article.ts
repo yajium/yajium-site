68eab23e9511f7a7f6298c7a5af510595633768c
yajium <117247060+yajium@users.noreply.github.com>
2023-02-19 23:01:47 +0900

    fix: details

--- src/content/schemas/article.ts
+++ src/content/schemas/article.ts
@@ -3,15 +3,10 @@ import { z } from "astro/zod";
 export const articleSchema = z.object({
   title: z.string(),
   description: z.string(),
-  // Transform string to Date object
   pubDate: z
     .string()
     .or(z.date())
     .transform((val) => new Date(val)),
   pubFromatedDate: z.string().optional(),
-  // updatedDate: z
-  //   .string()
-  //   .optional()
-  //   .transform((str) => (str ? new Date(str) : undefined)),
   tags: z.array(z.string()),
 });
