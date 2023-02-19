574cf6e223cfa73313fa08781c7c019c9b74fee1
yajium <117247060+yajium@users.noreply.github.com>
2023-02-19 18:29:31 +0900

    fix: error

--- src/components/util/PostBody.tsx
+++ src/components/util/PostBody.tsx
@@ -1,13 +1,15 @@
 import "zenn-content-css";
 import remarkToc from "remark-toc";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import rehypeSlug from "rehype-slug";
+import markdownToHtml from "zenn-markdown-html";
 
 type Props = {
   content: string;
 };
 
 const PostBody = ({ content }: Props) => {
+  content = markdownToHtml(content);
+
   return (
     <div className="znc my-4 px-6 py-6 sm:px-20 sm:py-20 mx-5 text-sm sm:text-base sm:mx-auto rounded-3xl bg-beige">
       <div dangerouslySetInnerHTML={{ __html: content }} />
