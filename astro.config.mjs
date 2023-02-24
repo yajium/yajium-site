7ea111ce734b307ed51d89bddb83dfeaa30a2fff
yajium <117247060+yajium@users.noreply.github.com>
2023-02-24 23:00:52 +0900

    fix: file settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -9,5 +10,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  integrations: [mdx(), sitemap(), react(), tailwind(), image()],
 });
